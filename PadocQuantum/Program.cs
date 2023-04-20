6d8a13f998b4cbb6a587c34727ac1b85cf82e875
Quinten Croes <q.croes@ic-verzekeringen.be>
2023-04-20 16:00:42 +0200

    commit

--- PadocQuantum/Program.cs
+++ PadocQuantum/Program.cs
@@ -9,30 +9,6 @@ namespace PatdocQuantum {
 
         [STAThread]
         static void Main() {
-            /* using (PatdocQuantumContext pqC = new PatdocQuantumContext()) {
-
-                 //pqC.Policy.Add(new Policy() { Name = "Test", Number = "T0001" });
-                 pqC.SaveChanges();
-
-
-
-                 var policies = pqC.Policy.ToList();
-             }*/
-
-            var usersWithAllPermission = new PatdocQuantumContext().User
-    .Where(u => u.UserRole
-        .Any(ur => ur.Role.RolePermission
-            .Any(rp => rp.Permission.Name == "ALL")))
-    .ToQueryString();
-
-            var policiesOfClient004 = new PatdocQuantumContext().Policy
-    .Where(p => p.ClientId == "004")
-    .ToQueryString();
-
-
-            var options = new DbContextOptionsBuilder<PatdocQuantumContext>()
-                .Options;
-
 
             Thread.CurrentThread.CurrentCulture = new CultureInfo("nl");
 
